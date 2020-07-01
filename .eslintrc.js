d9d6b5dcaad4c946f8f21805454b3651c38bef09
Sam <hatem@clearrain.xyz>
2020-07-01 19:33:29 -0400

    feat: added validation from rain-js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,8 @@ module.exports = {
     es6: true
   },
   extends: "standard",
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module"
   }
-};
+}
---
+++ index.js
@@ -0,0 +1,119 @@
+/*
+    hooverd: A simple Arweave signing and dispatch server.
+
+    After initilisation, a hooverd can be sent unsigned Arweave transactions
+    which it will then prepare, sign, and dispatch to the network.
+
+    Fields in the unsigned transaction can be ommited, making it extremely
+    simple to integrate without needing an Arweave aware client to the server.
+*/
+
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fs = require('fs')
+const Arweave = require('arweave/node')
+const argv = require('yargs').argv
+
+const { isValidTx } = require('../rain-js/dist/main')
+
+// Set Arweave parameters from commandline or defaults.
+const arweavePort = argv.arweavePort ? argv.arweavePort : 443
+const arweaveHost = argv.arweaveHost ? argv.arweaveHost : 'arweave.net'
+const arweaveProtocol = argv.arweaveProtocol ? argv.arweaveProtocol : 'https'
+
+// Set hooverd parameters.
+const port = argv.port ? argv.port : 1908
+
+const walletPath = process.env.WALLET_PATH || argv.walletFile
+
+if (!walletPath) {
+  console.log('ERROR: Please specify a wallet file to load using argument ' +
+          "'--wallet-file <PATH>'.")
+  process.exit()
+}
+
+const rawWallet = fs.readFileSync(walletPath)
+const wallet = JSON.parse(rawWallet)
+
+const arweave = Arweave.init({
+  host: arweaveHost, // Hostname or IP address for a Arweave node
+  port: arweavePort,
+  protocol: arweaveProtocol
+})
+
+async function handleRequest (req, res, next) {
+  try {
+    if (!isValidTx(req.body)) {
+      res.status(400).send({ error: 'Arweave Proxy Upload Service: Invalid transaction provided' })
+      return
+    }
+
+    const tx = await arweave.createTransaction({ data: req.body.jwt }, wallet)
+
+    const tags = req.body.tags
+
+    Object.keys(tags).forEach(key => {
+      tx.addTag(key, tags[key])
+    })
+
+    dispatchTX(tx, res)
+  } catch (e) {
+    console.log(e, 'THE ERROR')
+
+    next(e)
+  }
+}
+
+async function dispatchTX (tx, res) {
+  // Manually set the transaction anchor, for now.
+  const anchorId = await arweave.api.get('/tx_anchor').then(x => x.data)
+  tx.last_tx = anchorId
+
+  // Sign and dispatch the TX, forwarding the response code as our own.
+  await arweave.transactions.sign(tx, wallet)
+  const resp = await arweave.transactions.post(tx)
+
+  const output = `Transaction ${tx.get('id')} dispatched to ` +
+          `${arweaveHost}:${arweavePort} with response: ${resp.status}.`
+  console.log(output)
+
+  res.status(resp.status).send(output)
+}
+
+async function startServer () {
+  console.log('Welcome to hooverd! 👋\n\nWe are...')
+
+  // Print introductory information to the console.
+  console.log(`...starting a server at http://localhost:${port}.`)
+
+  const address = await arweave.wallets.jwkToAddress(wallet)
+  const balance = arweave.ar.winstonToAr(await arweave.wallets.getBalance(address))
+  console.log(`...using wallet ${address} (balance: ${balance} AR).`)
+
+  const netInfo = await arweave.network.getInfo()
+  console.log('...dispatching transactions to Arweave host at',
+          `${arweaveHost}:${arweavePort},`,
+          `synchronised at block ${netInfo.height}.`)
+
+  // Start the server itself.
+  const app = express()
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+  app.post('/', handleRequest)
+
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log('Server experienced error:', err)
+    }
+
+    console.log('...now ready to hoover data! 🚀🚀🚀\n')
+  })
+}
+
+startServer()
